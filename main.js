11b68bbb7277a694f7e289f64c9ffe0afdbecdb0
Avanya Haldar <82445699+AvanyaHaldar@users.noreply.github.com>
2022-03-07 15:45:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-function setup(){
-    canvas=createCanvas(500,450);
+status = "";
+input = "";
+function setup() {
+    canvas = createCanvas(500, 450);
     canvas.center();
+    video = createCapture(500, 450);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 500, 450);
+}
+
+function start() {
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    input = document.getElementById("name_of_objects").value;
+    console.log("Input = " + input);
+
+}
+
+function modelLoaded() {
+    console.log("Coco SSD Is Initialized");
+    status = true;
 }
